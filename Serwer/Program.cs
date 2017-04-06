3fc92b7da9d55e44922c732db0b4d056f325939d
bartoszdominiak <bartas199@o2.pl>
2017-04-06 09:13:47 +0200

    initial commit

---
+++ Serwer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server serwer = new Server(1000);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Serwer/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Serwer
+{
+    
+    class Users
+    {
+        public int nr { get; set; }
+        public string IP { get; set; }
+        public Client client { get; set; }
+
+        public Users(int new_nr, string new_ip, TcpClient new_tcp)
+        {
+            nr = new_nr;
+            IP = new_ip;
+            client = new Client(new_tcp, "1");
+        } 
+    }
+}
